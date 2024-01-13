d5ae8712ee352fa322ef7369f52d05a1771bd9ef
ducnmhe150286 <97494895+ducnmhe150286@users.noreply.github.com>
2024-01-13 19:49:10 +0700

    Thêm file .gitignore

---
+++ SWP/SWP/Models/FeedBack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SWP.Models
+{
+    public partial class FeedBack
+    {
+        public int FeedBackId { get; set; }
+        public int? UserId { get; set; }
+        public int? DetailId { get; set; }
+        public string? Comment { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int? Status { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? UpdatedBy { get; set; }
+
+        public virtual User? User { get; set; }
+    }
+}
