bfdeb28447057e38daabd4325fddd1704529de2d
Le Thanh <lekhathanh2001@gmail.com>
2024-02-28 00:02:55 +0700

    Add create Blog
    Add Create Blog and Edit Blog

---
+++ SWP/SWP/Dto/Request/Blogs/BlogRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace SWP.Dto.Request.Blogs
+{
+    public class BlogRequest
+    {
+        public int BlogId { get; set; }
+        public int? UserId { get; set; }
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public int? Status { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? UpdatedBy { get; set; }
+
+    }
+    public class CreateBlogRequest
+    { 
+
+    }
+
+    }
