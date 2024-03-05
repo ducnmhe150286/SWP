1ef2ad2d7a2e53d1518912aede783cfaccc2d38b
PhucNguyennvp <nguyenphucnvpvn@gmail.com>
2024-03-06 00:09:03 +0700

    update

--- SWP/SWP/Controllers/CartController.cs
+++ SWP/SWP/Controllers/CartController.cs
@@ -31,9 +31,9 @@ namespace SWP.Controllers
         public IActionResult GetCartItem(int? message)
         {
             var customer = HttpContext.Session.GetString("USER_EMAIL");
-            /*if (message == 1) {
+            if (message == 1) {
                 ViewData["message"] = "Vui long chon san pham thanh toán";
-            }*/
+            }
             var cusId = userDao.GetUserByEmail(customer);
             if (cusId is not null && cusId.RoleId == 2)
             {
