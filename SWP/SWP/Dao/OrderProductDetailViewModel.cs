bbc82538fb93ef67a5a4dc570bcc37d17a3c8cff
QuyNguyen <113528273+NguyenHuyQuy@users.noreply.github.com>
2024-03-12 04:31:13 +0700

    Manage Order

---
+++ SWP/SWP/Dao/OrderProductDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SWP.Dao
+{
+    public class OrderProductDetailViewModel
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string ColorName { get; set; }
+        public string SizeName { get; set; }
+        public List<string> ProductImages { get; set; }
+    }
+}
