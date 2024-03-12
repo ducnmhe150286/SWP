a55a9e7a850992bc1909474310f300a8d672f404
PhucNguyennvp <nguyenphucnvpvn@gmail.com>
2024-03-12 21:35:04 +0700

    add order history

---
+++ SWP/SWP/Controllers/OrderHistoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SWP.Dao;
+
+namespace SWP.Controllers
+{
+    public class OrderHistoryController : Controller
+    {
+        public UsersDao userDao;
+        public OrderHistoryDao historyDao;
+        public OrderHistoryController()
+        {
+            userDao = new UsersDao();
+            historyDao = new OrderHistoryDao();
+        }
+        public IActionResult Index()
+        {
+            var customer = HttpContext.Session.GetString("USER_EMAIL");
+
+            var cusId = userDao.GetUserByEmail(customer);
+            if (cusId is not null && cusId.RoleId == 2)
+            {
+                var getOrder = historyDao.getAllOrder(cusId.UserId);
+                ViewData["listItem"] = getOrder;
+                return View();
+            }
+                return RedirectToAction("Index","Auth");
+        }
+    }
+}
