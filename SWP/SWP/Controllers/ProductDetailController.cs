0d9c45f08692f34aeea9328ae6b73cbcab89b1b7
Le Thanh <lekhathanh2001@gmail.com>
2024-03-20 00:32:46 +0700

    Update feecback
    Update feecback

--- SWP/SWP/Controllers/ProductDetailController.cs
+++ SWP/SWP/Controllers/ProductDetailController.cs
@@ -27,20 +27,25 @@ namespace SWP.Controllers
             string email = HttpContext.Session.GetString("USER_EMAIL");
             var pro = proDetail.getProductById(productId);
             var listProSimilar = proDetail.getProductByCategory(pro);
-            bool check = email != null && context.Orderdetails.Where(n => n.Detail.ProductId == productId && n.Order.User.Email == email).Count() >= 1;
+            bool check = email != null && context.Orderdetails.Where(n => n.Detail.ProductId == productId && n.Order.User.Email == email).Count() > 0 
+                && context.FeedBacks.Where(n => n.Product.ProductId == productId && n.User.Email == email).Count() == 0;
             ViewData["listPro"] = listProSimilar;
             ViewData["error_quantity"] = TempData["error_quantity"];
             List<FeedBack> feedBacks = context.FeedBacks.Include(n => n.User).Where(n => n.Product.ProductId == productId).ToList();
             ViewData["listPro"] = listProSimilar;
             ViewBag.check = check;
             ViewBag.listFeedback = feedBacks;
-            if(feedBacks != null)
+            if(feedBacks != null && feedBacks.Count() != 0)
             {
                 ViewBag.isUser = email == feedBacks[0].User.Email;
             }
-           
-            
-           
+            else
+            {
+                ViewBag.isUser = false; 
+            }
+
+
+
             return View(pro);
         }
         
